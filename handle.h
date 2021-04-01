0d0c49240336f1aab7f8fda0b4bba3a17192d830
泽宇 张 <zeyuzhang@88.com>
2021-04-01 09:35:04 +0800

    修正

--- handle.h
+++ handle.h
@@ -15,21 +15,39 @@ int getMax(const int *arr, int count) {
 
 }
 
+void quickSort(int a[], int l, int r) {
+    if (l >= r)
+        return;
+    int i = l;
+    int j = r;
+    int key = a[l];//选择第一个数为key
+    while (i < j) {
+        while (i < j && a[j] >= key)//从右向左找第一个小于key的值
+            j--;
+        if (i < j) {
+            a[i] = a[j];
+            i++;
+        }
+        while (i < j && a[i] < key)//从左向右找第一个大于key的值
+            i++;
+        if (i < j) {
+            a[j] = a[i];
+            j--;
+        }
+    }
+    a[i] = key;
+    quickSort(a, l, i - 1);//继续排左部分，递归调用
+    quickSort(a, i + 1, r);//继续排右部分，递归调用
+}
+
 //奇数个数据求中间值
 int getMiddle(const int *arr, int count) {
     int *copy = (int *) malloc(sizeof(int) * count);
     for (int i = 0; i < count; i++) {
         copy[i] = arr[i];
     }
-    for (int i = 0; i < count - 1; i++) {
-        if (copy[i] > copy[i + 1]) {
-            int temp;
-            temp = copy[i];
-            copy[i] = copy[i + 1];
-            copy[i + 1] = temp;
-        }
-    }
-    return copy[count / 2] + 1;
+    quickSort(copy, 0, count - 1);
+    return copy[count / 2 + 1];
 }
 
 //24位分色
